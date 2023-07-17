99aa7e585895d2b1cea35fb0230fa080c9288559
sarath <sarath@gmail.com>
2023-07-18 00:00:45 +0530

    temp file

--- Shell/Roboshop-shell/frontend.sh
+++ Shell/Roboshop-shell/frontend.sh
@@ -1,24 +1,24 @@
 echo -e "\e[33mInstalling Nginx\e[0m"
-yum install nginx -y &>>temp/ngnix.log
+yum install nginx -y &>> /tmp/ngnix.log
 
 echo -e "\e[33enabling ngnix and starting\e[0m"
-systemctl enable nginx &>>temp/ngnix.log
-systemctl start nginx &>>temp/ngnix.log
+systemctl enable nginx &>> /tmp/ngnix.log
+systemctl start nginx &>> /tmp/ngnix.log
 
 echo -e "\e[33mRemoving old content\e[0m"
-rm -rf /usr/share/nginx/html/* &>>temp/ngnix.log
+rm -rf /usr/share/nginx/html/* &>> /tmp/ngnix.log
 
 echo -e "\e[33mDownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>temp/ngnix.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/ngnix.log
 
 echo -e "\e[33mUnzipping files\e[0m"
-cd /usr/share/nginx/html &>>temp/ngnix.log
-unzip /tmp/frontend.zip  &>>temp/ngnix.log
+cd /usr/share/nginx/html &>> /tmp/ngnix.log
+unzip /tmp/frontend.zip  &>> /tmp/ngnix.log
 
 echo -e "\e[33mCopying config file\e[0m"
 
-cp DevOps\Shell\Roboshop-shell\roboshop.config  /etc/nginx/default.d/roboshop.conf &>>temp/ngnix.log
+cp DevOps\Shell\Roboshop-shell\roboshop.config  /etc/nginx/default.d/roboshop.conf &>> /tmp/ngnix.log
 
 
 echo -e "\e[33mRestarting server\e[0m"
-systemctl restart nginx &>>temp/ngnix.log
+systemctl restart nginx &>>/tmp/ngnix.log
