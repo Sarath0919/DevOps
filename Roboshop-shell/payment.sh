250611ff02ebbd96113a26d8e715c4355a5272f0
sarath <sarath@gmail.com>
2023-08-02 01:12:14 +0530

    changes

--- Roboshop-shell/payment.sh
+++ Roboshop-shell/payment.sh
@@ -3,7 +3,7 @@ component="payment"
 
 roboshop_app_pwd=$1
 
-if [-z "$roboshop_app_pwd"]; then
+if [ -z "$roboshop_app_pwd" ]; then
     echo  password is required
     exit 1
 fi
--- Roboshop-shell/shipping.sh
+++ Roboshop-shell/shipping.sh
@@ -3,7 +3,7 @@ component="shipping"
 
 mysql_root_pwd=$1
 
-if [-z "$mysql_root_pwd"]; then
+if [ -z "$mysql_root_pwd" ]; then
     echo Mysql password is required
     exit 1
 fi
